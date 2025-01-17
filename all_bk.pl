38d3cbcf42e7320d28405d3f844a95b507a977f7
Lun Ai <lun.ai15@imperial.ac.uk>
2025-01-17 11:39:42 +0000

    Combine bk from all circuits and update gate identifier numbers

--- all_bk.pl
+++ all_bk.pl
@@ -1,158 +1,60 @@
-gate(101).
-gate(102).
-gate(103).
-gate(104).
-gate(201).
-gate(202).
-gate(203).
-gate(204).
-gate(205).
-gate(301).
-gate(302).
-gate(303).
-gate(304).
-gate(305).
-gate(306).
-gate(401).
-gate(402).
-gate(403).
-gate(404).
-gate(405).
-gate(406).
-gate(407).
-gate(501).
-gate(502).
-gate(503).
-gate(504).
-gate(505).
-gate(506).
-gate(507).
-gate(601).
-gate(602).
-gate(603).
-gate(604).
-gate(605).
-gate(606).
-gate(607).
-gate(608).
-gate(701).
-gate(702).
-gate(703).
-gate(704).
-gate(705).
-gate(706).
-gate(707).
-gate(708).
-gate(709).
-gate(710).
-gate(801).
-gate(802).
-gate(803).
-gate(901).
-gate(902).
-gate(903).
-gate(904).
-gate(1001).
-gate(1002).
-gate(1003).
-gate(1004).
-gate(1005).
-gate(1101).
-gate(1102).
-gate(1103).
-gate(1104).
-gate(1105).
-gate(1106).
-gate(1201).
-gate(1202).
-gate(1203).
-gate(1204).
-gate(1205).
-gate(1206).
-gate(1301).
-gate(1302).
-gate(1303).
-gate(1304).
-gate(1305).
-gate(1306).
-gate(1307).
-gate(1401).
-gate(1402).
-gate(1403).
-gate(1404).
-gate(1405).
-gate(1406).
-gate(1407).
-gate(1408).
-is_connected(101,102).
-is_connected(101,103).
-is_connected(102,104).
-is_connected(202,203).
-is_connected(203,204).
-is_connected(201,205).
-is_connected(204,205).
-is_connected(301,303).
-is_connected(302,303).
-is_connected(303,304).
-is_connected(304,305).
-is_connected(305,306).
-is_connected(401,405).
-is_connected(402,405).
-is_connected(403,406).
-is_connected(404,406).
-is_connected(405,407).
-is_connected(501,502).
-is_connected(502,503).
-is_connected(503,504).
-is_connected(503,505).
-is_connected(504,506).
-is_connected(505,507).
-is_connected(601,603).
-is_connected(602,603).
-is_connected(602,604).
-is_connected(602,605).
-is_connected(603,606).
-is_connected(604,607).
-is_connected(606,608).
-is_connected(701,702).
-is_connected(701,703).
-is_connected(702,704).
-is_connected(703,705).
-is_connected(704,706).
-is_connected(702,707).
-is_connected(705,707).
-is_connected(705,708).
-is_connected(707,709).
-is_connected(709,710).
-is_connected(801,802).
-is_connected(802,803).
-is_connected(901,902).
-is_connected(902,904).
-is_connected(903,904).
-is_connected(1001,1004).
-is_connected(1002,1004).
-is_connected(1002,1005).
-is_connected(1003,1005).
-is_connected(1101,1104).
-is_connected(1102,1104).
-is_connected(1103,1105).
-is_connected(1104,1106).
-is_connected(1105,1106).
-is_connected(1202,1203).
-is_connected(1201,1204).
-is_connected(1203,1204).
-is_connected(1204,1205).
-is_connected(1205,1206).
-is_connected(1301,1302).
-is_connected(1301,1303).
-is_connected(1302,1304).
-is_connected(1304,1305).
-is_connected(1303,1306).
-is_connected(1305,1306).
-is_connected(1306,1307).
-is_connected(1401,1403).
-is_connected(1403,1404).
-is_connected(1402,1405).
-is_connected(1404,1406).
-is_connected(1405,1407).
-is_connected(1406,1408).
+% General BK for all circuits
+% Find the smaller and larger number of two numbers
+min(A, B, C) :- min_list([A,B],C).
+max(A, B, C) :- max_list([A,B],C).
+
+% Is a number A greater than / less than or equal to another number in a triplet
+gt(A, B) :- A > B.
+leq(A, B) :- A =< B.
+
+% A gate F partitions the circuit: gates on the same branch as F and gates on different branches
+same_circuit(X, Y) :- gate(X), gate(Y), N is X // 100, M is Y // 100, N == M.
+partition(F, P1, P2) :- findall(X, (gate(X), same_circuit(X, F), on_same_branch(X, F)), P1), 
+                        findall(X, (gate(X), same_circuit(X, F), \+ on_same_branch(X, F)), P2).
+
+% Are multiple gates connected to gate X?
+has_branches(X) :- is_connected(X, Y), is_connected(X, Z), Y \== Z.
+
+% Is gate X on the same branch as Y where X is an upstream gate?
+on_same_branch(X, X).
+on_same_branch(X, Y) :- is_connected(X, Z), \+ has_branches(X), on_same_branch(Z, Y).
+
+% Compute the minimum size of partitions
+min_size(P1, P2, N) :- length(P1, N1), length(P2, N2), min(N1, N2, N).
+max_size(P1, P2, N) :- length(P1, N1), length(P2, N2), max(N1, N2, N).
+
+% Given a fault, compute the minimum and maximum partition sizes
+partition_sizes(F, [Min, Max]) :- partition(F, P1, P2), min_size(P1, P2, Min), 
+                                  min_size(P1, P2, Max).
+min_partition_size(F, N) :- partition_sizes(F, [N, _]).
+
+% Return the partitions with a larger min partition size
+max_min_size([A,B], [C,D], [A,B]) :- not_list(A), not_list(B), not_list(C), not_list(D), max(A, C, A).
+max_min_size([A,B], [C,D], [C,D]) :- not_list(A), not_list(B), not_list(C), not_list(D), max(A, C, C).
+
+% Not a list
+not_list(A) :- \+ is_list(A).
+
+% List manipulation predicates
+fold(_P,Acc,[],Acc).
+fold(P,Acc,[H|T],Out) :- 
+    call(P,Acc,H,Inter),
+    fold(P,Inter,T,Out).
+
+map(_P,[],[]).
+map(P,[H|T],[H1|T1]) :- 
+    call(P,H,H1),
+    map(P,T,T1).
+
+% Get the head/tail of a list
+tail([_|T],T).
+head([H|_],H).
+
+% Get the head and tail of a list
+head_fault(In, H) :- fst(In, List), head(List, H).
+tail_faults(In, T) :- fst(In, List), tail(List, T).
+
+empty([]).
+% zero(0).
+empty_partitions([0, 0]).
+eq(A, A).
--- test_acc.pl
+++ test_acc.pl
@@ -1,3 +1,7 @@
+:- ['select_test.pl'].
+
+ex_ids([1,2,3,4,5,6,7,8,9,10,11,12,13,14]).
+
 opt_tests(ex_1, ['d']).
 opt_tests(ex_2, ['c', 'd']).
 opt_tests(ex_3, ['c']).
@@ -13,12 +17,31 @@ opt_tests(ex_12, ['c', 'd']).
 opt_tests(ex_13, ['d', 'e']).
 opt_tests(ex_14, ['f']).
 
-concat_bk(ExNums) :-
-    tell('all_bk.pl'),
-    forall(member(N, ExNums), concat_bk_1(N)),
-    forall(member(N, ExNums), concat_bk_2(N)),
-    told.
+opt_gate_split(ex_1,[104]).
+opt_gate_split(ex_2,[203,204]).
+opt_gate_split(ex_3,[303]).
+opt_gate_split(ex_4,[405,406,407]).
+opt_gate_split(ex_5,[503]).
+opt_gate_split(ex_6,[608]).
+opt_gate_split(ex_7,[710]).
+opt_gate_split(ex_8,[801,802]).
+opt_gate_split(ex_9,[902]).
+opt_gate_split(ex_10,[1004,1005]).
+opt_gate_split(ex_11,[1104]).
+opt_gate_split(ex_12,[1203,1204]).
+opt_gate_split(ex_13,[1304,1305]).
+opt_gate_split(ex_14,[1406]).
 
+concat_bk :-
+    ex_ids(Ids),
+    tell('ex_bk.pl'),
+    forall(member(N, Ids), concat_bk_1(N)),
+    forall(member(N, Ids), concat_bk_2(N)),
+    told,
+    forall(member(N, Ids), print_opt_test_gates(N)).
+
+% Convert ex_i/bk.pl to ex_bk.pl
+% New gate numbers in ex_bk.pl = i * 100 + original gate number in ex_i/bk.pl
 concat_bk_1(N) :-
     atomic_list_concat(['ex_', N, '/bk.pl'], SrcPath),
     consult(SrcPath),
@@ -29,41 +52,32 @@ concat_bk_2(N) :-
     consult(SrcPath),
     forall((is_connected(X, Y), gate(X), gate(Y)), (NewX is N * 100 + X, NewY is N * 100 + Y, write(is_connected(NewX, NewY)), writeln('.'))),
     unload_file(SrcPath).
-
-% test(Src, Cnt) :-
-%     atomic_list_concat([Src, '/bk.pl'], SrcPath),
-%     consult(SrcPath),
-%     consult('bk.pl'),
-%     opt_tests(Src, Ts),
-%     findall(G, gate(G), Gates),
-%     select_test(Gates, N, G),
-%     length(Gates, L),
-%     N1 is L - N,
-%     out(G,Out),
-%     (member(Out, Ts) -> Cnt = 1; Cnt = 0),
-%     format("Circuit No. ~w, test: ~w, split: ~w/~w, Optimal: ~w\n", [Src, Out, N, N1, Cnt]),
-%     writeln(select_test(Gates, N, G)),
-%     unload_file('bk.pl'),
-%     unload_file(SrcPath),!.
-
-test(Src, Cnt) :- 
+print_opt_test_gates(N) :-
+    atomic_list_concat(['ex_', N], Src),
     atomic_list_concat([Src, '/bk.pl'], SrcPath),
     consult(SrcPath),
-    consult('bk.pl'),
-    opt_tests(Src, Ts),
-    findall(G, gate(G), Gates),
+    opt_tests(Src, OptTests),
+    findall(G1,(member(T,OptTests),out(G, T), G1 is 100 * N + G),Gs),
+    format("~w.\n", [opt_gate_split(Src,Gs)]),
+    unload_file(SrcPath).
+
+test(CircuitNum, Src, Cnt) :- 
+    opt_gate_split(Src, Gs),
+    findall(G, (gate(G), CircuitNum is G // 100), Gates),
     select_test(Gates, [N, N1]),
-    findall(G, (member(T, Ts), out(G, T)), Gs),
     maplist(partition_sizes, Gs, Ps),
     (member([N, N1], Ps) -> Cnt = 1; Cnt = 0),
     format("Circuit No. ~w, split: ~w/~w, Optimal: ~w\n", [Src, N, N1, Cnt]),
-    writeln(select_test(Gates, (N, N1))),
-    unload_file('bk.pl'),
-    unload_file(SrcPath),!.
+    format("\tResult: ~w\n", [select_test(Gates, (N, N1))]),!.
 
 test_acc :-
-    unload_file('all_bk.pl'),
-    findall(Cnt, (opt_tests(Src, _), test(Src, Cnt)), Cnts),
+    ex_ids(Ids),
+    findall(Cnt,    
+                (member(CircuitNum,Ids),
+                atomic_list_concat(['ex_',CircuitNum], Src),
+                test(CircuitNum, Src, Cnt)
+                ), 
+            Cnts),
     sum_list(Cnts, Sum),
     length(Cnts, N),
     Acc is Sum / N,
